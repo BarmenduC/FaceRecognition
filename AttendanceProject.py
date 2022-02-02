5a0bb8adba5c9dab942c86f486b8453e30d560aa
Barmendu Chakraborty <officialbarmenduchakraborty@gmail.com>
2022-02-02 18:59:42 +0530

    Split Text applied

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -8,9 +8,14 @@ images = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
 
 
-imgElon = face_recognition.load_image_file('ImageBasic/images.jpg')
-imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImageBasic/images (2).jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+#imgElon = face_recognition.load_image_file('ImageBasic/images.jpg')
+#imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+#imgTest = face_recognition.load_image_file('ImageBasic/images (2).jpg')
+#imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
