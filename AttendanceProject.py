c343dd755cbb13171fb7fedf22ba58256e4cc108
Barmendu Chakraborty <officialbarmenduchakraborty@gmail.com>
2022-02-02 21:13:05 +0530

    Automatic List preparation

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -18,5 +18,22 @@ def findEncodings(images):
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print(len(encodeListKnown))
 
 
+
+#faceLoc = face_recognition.face_locations(imgElon)[0]
+#encodeElon = face_recognition.face_encodings(imgElon)[0]
+#cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+
+#faceLoc = face_recognition.face_locations(imgTest)[0]
+#encodeTest = face_recognition.face_encodings(imgTest)[0]
+#cv2.rectangle(imgTest,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+
+#results = face_recognition.compare_faces([encodeElon],encodeTest)
+#faceDis = face_recognition.face_distance([encodeElon],encodeTest)
