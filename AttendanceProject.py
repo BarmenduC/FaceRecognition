a2ce7c9ce866244e7e15e3242a1788564e531091
Barmendu Chakraborty <officialbarmenduchakraborty@gmail.com>
2022-02-03 09:10:34 +0530

    Encoding current frame in case of multiple faces

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -30,8 +30,10 @@ cap = cv2.VideoCapture(0)
 while true:
     success, img = cap.read()
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
 
 
 #faceLoc = face_recognition.face_locations(imgElon)[0]
